58ca95ee0d409ce45dbc92f21a84c14fd42b2ec4
Aaron Claydon <claydon.aaron@gmail.com>
2022-01-04 21:55:56 +0000

    Remove comments with the now disabled Slack token -_-

--- cmd/notify/main.go
+++ cmd/notify/main.go
@@ -44,17 +44,6 @@ func main() {
 
 					server.SetupServer(listenAddress)
 
-					// runtime.ProcessEvent(config.Global.Events[0], map[string]interface{}{
-					// 	"stop_groups": map[string]interface{}{
-					// 		"inserts": float64(4),
-					// 		"updates": float64(3),
-					// 	},
-					// 	"stops": map[string]interface{}{
-					// 		"inserts": float64(11),
-					// 		"updates": float64(18),
-					// 	},
-					// })
-
 					return nil
 				},
 			},
@@ -65,31 +54,4 @@ func main() {
 	if err != nil {
 		log.Fatal().Err(err).Send()
 	}
-
-	/*
-		api := slack.New("xoxb-2896435494805-2892761762070-VUF7XabghpBauGMYB5hLFlrM")
-		attachment := slack.Attachment{
-			Title:   "wowowow",
-			Pretext: "some pretext",
-			Text:    "some text",
-			Fields: []slack.AttachmentField{
-				{
-					Title: "What the hell",
-					Value: "no",
-				},
-			},
-		}
-
-		channelID, timestamp, err := api.PostMessage(
-			"C02T53DLZ4G",
-			slack.MsgOptionText("Some text", false),
-			slack.MsgOptionAttachments(attachment),
-			slack.MsgOptionAsUser(true), // Add this if you want that the bot would post message as a user, otherwise it will send response using the default slackbot
-		)
-		if err != nil {
-			fmt.Printf("%s\n", err)
-			return
-		}
-		fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
-	*/
 }
